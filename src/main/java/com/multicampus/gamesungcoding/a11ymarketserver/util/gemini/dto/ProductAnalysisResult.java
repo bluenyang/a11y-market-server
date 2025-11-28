60476a6a3bb4b3b510cc1d01bfc7522a5e662c94
BlueNyang <dksrbxo@bluenyang.kr>
2025-11-28 13:00:39 +0900

    feat: add table for ai summary

--- src/main/java/com/multicampus/gamesungcoding/a11ymarketserver/util/gemini/dto/ProductAnalysisResult.java
+++ src/main/java/com/multicampus/gamesungcoding/a11ymarketserver/util/gemini/dto/ProductAnalysisResult.java
@@ -2,6 +2,5 @@ package com.multicampus.gamesungcoding.a11ymarketserver.util.gemini.dto;
 
 public record ProductAnalysisResult(String summary,
                                     String usageContext,
-                                    String usageMethod,
-                                    String category) {
+                                    String usageMethod) {
 }
