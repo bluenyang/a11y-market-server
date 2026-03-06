cdb5309219c5a7df52a5184ba51c1636595d0c3c
BlueNyang <xpsj20@gmail.com>
2026-03-06 21:49:00 +0900

    refactor: remove duplicated ResponseStatus
    Signed-off-by: BlueNyang <xpsj20@gmail.com>

--- src/main/kotlin/com/multicampus/gamesungcoding/a11ymarketserver/feature/auth/controller/AuthController.kt
+++ src/main/kotlin/com/multicampus/gamesungcoding/a11ymarketserver/feature/auth/controller/AuthController.kt
@@ -5,78 +5,82 @@ import com.multicampus.gamesungcoding.a11ymarketserver.common.jwt.dto.RefreshReq
 import com.multicampus.gamesungcoding.a11ymarketserver.feature.auth.dto.*
 import com.multicampus.gamesungcoding.a11ymarketserver.feature.auth.service.AuthService
 import com.multicampus.gamesungcoding.a11ymarketserver.feature.user.dto.UserResponse
+import jakarta.servlet.http.HttpServletResponse
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.web.bind.annotation.*
-import java.net.URI
 import java.util.*
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/v1/auth")
 class AuthController(
     private val authService: AuthService
 ) {
 
-    @PostMapping("/v1/auth/login")
-    fun login(@RequestBody dto: LoginRequest): ResponseEntity<LoginResponse> =
-        ResponseEntity.ok(authService.login(dto))
+    @PostMapping("/login")
+    fun login(@RequestBody dto: LoginRequest): LoginResponse =
+        authService.login(dto)
 
 
-    @PostMapping("/v1/auth/login-refresh")
-    fun loginRefresh(@Valid @RequestBody dto: RefreshRequest): ResponseEntity<LoginResponse> =
-        ResponseEntity.ok(authService.loginRefresh(dto.refreshToken))
+    @PostMapping("/login-refresh")
+    fun loginRefresh(@Valid @RequestBody dto: RefreshRequest): LoginResponse =
+        authService.loginRefresh(dto.refreshToken)
 
 
-    @PostMapping("/v1/auth/logout")
+    @PostMapping("/logout")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     fun logout(@AuthenticationPrincipal userDetails: UserDetails): ResponseEntity<Void> {
         authService.logout(userDetails.username)
         return ResponseEntity.noContent().build()
     }
 
-    @PostMapping("/v1/auth/refresh")
-    fun refreshToken(@Valid @RequestBody refreshRequest: RefreshRequest): ResponseEntity<JwtResponse> =
-        ResponseEntity.ok(authService.reissueToken(refreshRequest.refreshToken))
+    @PostMapping("/refresh")
+    fun refreshToken(@Valid @RequestBody refreshRequest: RefreshRequest): JwtResponse =
+        authService.reissueToken(refreshRequest.refreshToken)
 
 
-    @PostMapping("/v1/auth/join")
+    @PostMapping("/join")
     @ResponseStatus(HttpStatus.CREATED)
-    fun join(@RequestBody @Valid dto: JoinRequest): ResponseEntity<UserResponse> =
-        ResponseEntity
-            .created(URI.create("/api/v1/users/me"))
-            .body(authService.join(dto))
+    fun join(
+        @RequestBody @Valid dto: JoinRequest,
+        response: HttpServletResponse
+    ): UserResponse {
+        response.setHeader("Location", "/api/me")
+        return authService.join(dto)
+    }
 
-    @PostMapping("/v1/auth/kakao-join")
+    @PostMapping("/kakao-join")
     @ResponseStatus(HttpStatus.CREATED)
     fun kakaoJoin(
         @AuthenticationPrincipal principal: UserDetails,
-        @RequestBody dto: KakaoSignUpRequest
-    ): ResponseEntity<UserResponse> =
-        ResponseEntity
-            .created(URI.create("/api/v1/users/me"))
-            .body(authService.kakaoJoin(UUID.fromString(principal.username), dto))
+        @RequestBody dto: KakaoSignUpRequest,
+        response: HttpServletResponse
+    ): UserResponse {
+        response.setHeader("Location", "/api/me")
+        return authService.kakaoJoin(UUID.fromString(principal.username), dto)
+    }
 
-    @GetMapping("/v1/auth/me/info")
+    @GetMapping("/me/info")
     fun getLoginUserInfo(
         @AuthenticationPrincipal userDetails: UserDetails
-    ): ResponseEntity<LoginResponse> =
-        ResponseEntity.ok(authService.getUserInfo(UUID.fromString(userDetails.username)))
+    ): LoginResponse =
+        authService.getUserInfo(UUID.fromString(userDetails.username))
 
 
-    @GetMapping("/v1/auth/check/email")
+    @GetMapping("/check/email")
     fun checkEmail(
         @RequestParam email: String
-    ): ResponseEntity<CheckExistsResponse> =
-        ResponseEntity.ok(authService.isEmailDuplicate(email))
+    ): CheckExistsResponse =
+        authService.isEmailDuplicate(email)
 
-    @GetMapping("/v1/auth/check/phone")
-    fun checkPhone(@RequestParam phone: String): ResponseEntity<CheckExistsResponse> =
-        ResponseEntity.ok(authService.isPhoneDuplicate(phone))
+    @GetMapping("/check/phone")
+    fun checkPhone(@RequestParam phone: String): CheckExistsResponse =
+        authService.isPhoneDuplicate(phone)
 
-    @GetMapping("/v1/auth/check/nickname")
-    fun checkNickname(@RequestParam nickname: String): ResponseEntity<CheckExistsResponse> =
-        ResponseEntity.ok(authService.isNicknameDuplicate(nickname))
+    @GetMapping("/check/nickname")
+    fun checkNickname(@RequestParam nickname: String): CheckExistsResponse =
+        authService.isNicknameDuplicate(nickname)
 }
